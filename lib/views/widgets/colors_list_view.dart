ea58c92c9a124291fb0f351c3e11d64f1c45d1c4
MohamedRomana <MohamedRomana124@gmail.com>
2024-09-27 17:53:22 +0300

    create colors list part two

--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,29 +1,61 @@
 import 'package:flutter/material.dart';
 
 class ColorsItem extends StatelessWidget {
-  const ColorsItem({super.key});
-
+  const ColorsItem({super.key, required this.isActive, required this.color});
+  final bool isActive;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 38,
-      backgroundColor: Colors.blue,
-    );
+    return isActive
+        ? CircleAvatar(
+            radius: 38,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 34,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 38,
+            backgroundColor: color,
+          );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = [
+    const Color(0xffEE6055),
+    const Color(0xff60D394),
+    const Color(0xffAAF683),
+    const Color(0xffFFD97D),
+    const Color(0xffFF9B85),
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 38 * 2,
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return const ColorsItem();
+          return GestureDetector(
+            onTap: () {
+              currentIndex = index;
+              setState(() {});
+            },
+            child: ColorsItem(
+              isActive: currentIndex == index,
+              color: colors[index],
+            ),
+          );
         },
       ),
     );
